b0433296139f62362046cd740b0bb6a93f83faae
Sam Giffney <samuelgiffney@gmail.com>
2024-07-28 12:50:20 +1000

    Use headers on CSVs (#2)

--- scripts/export-street-postcodes.sh
+++ scripts/export-street-postcodes.sh
@@ -6,7 +6,7 @@ set -eof
 echo "-- Exporting nz_street_postcodes"
 for x in {a..z}
 do
-  psql -d open_nz_postcodes -c "COPY (SELECT road_id, postcode, name, locality, city FROM nz_street_postcodes WHERE name ~* '^${x}' ORDER BY name, locality, city, road_id) TO '/app/street_postcodes/${x}.csv' WITH DELIMITER ',' csv;"
+  psql -d open_nz_postcodes -c "COPY (SELECT road_id, postcode, name, locality, city FROM nz_street_postcodes WHERE name ~* '^${x}' ORDER BY name, locality, city, road_id) TO '/app/street_postcodes/${x}.csv' WITH DELIMITER ',' HEADER csv;"
 done
 # use 0.csv for any roads starting with a digit
-psql -d open_nz_postcodes -c "COPY (SELECT road_id, postcode, name, locality, city FROM nz_street_postcodes WHERE name ~* '^\d' ORDER BY name, locality, city, road_id) TO '/app/street_postcodes/0.csv' WITH DELIMITER ',' csv;"
+psql -d open_nz_postcodes -c "COPY (SELECT road_id, postcode, name, locality, city FROM nz_street_postcodes WHERE name ~* '^\d' ORDER BY name, locality, city, road_id) TO '/app/street_postcodes/0.csv' WITH DELIMITER ',' HEADER csv;"
