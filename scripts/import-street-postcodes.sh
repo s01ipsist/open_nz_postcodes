34995f88fdccb06c81a24a37562d38e0275717dd
Sam Giffney <samuelgiffney@gmail.com>
2024-07-28 12:49:19 +1000

    Use headers on CSVs

--- scripts/import-street-postcodes.sh
+++ scripts/import-street-postcodes.sh
@@ -6,7 +6,7 @@ psql -d open_nz_postcodes -c "CREATE TABLE IF NOT EXISTS nz_street_postcodes (ro
 psql -d open_nz_postcodes -c "TRUNCATE TABLE nz_street_postcodes;"
 for x in {0,{a..z}}
 do
-  psql -d open_nz_postcodes -c "COPY nz_street_postcodes (road_id, postcode, name, locality, city) FROM '/app/street_postcodes/${x}.csv' DELIMITER ',' CSV;"
+  psql -d open_nz_postcodes -c "COPY nz_street_postcodes (road_id, postcode, name, locality, city) FROM '/app/street_postcodes/${x}.csv' DELIMITER ',' HEADER csv;"
 done
 psql -d open_nz_postcodes -c "CREATE INDEX IF NOT EXISTS nz_street_postcodes_road_idx ON nz_street_postcodes USING btree (road_id);"
 psql -d open_nz_postcodes -c "SELECT COUNT(*) FROM nz_street_postcodes;"
