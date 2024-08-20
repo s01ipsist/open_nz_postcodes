ab2a03898c60552520ec07848c59c3ad8376b2a8
Sam Giffney <samuelgiffney@gmail.com>
2024-08-20 11:08:47 +1000

    Fix extract path (#8)
    * fix meshblocks path
    
    * add push trigger for testing
    
    * remove debug

--- scripts/import-geodata.sh
+++ scripts/import-geodata.sh
@@ -23,7 +23,7 @@ path_addresses_1=data/nz-addresses/nz-addresses.shp
 path_addresses_2=data/nz-addresses/nz-addresses.2.shp
 path_roads=data/nz-roads-addressing/nz-roads-addressing.shp
 path_suburbs=data/nz-suburbs-and-localities/nz-suburbs-and-localities.shp
-path_meshblocks=data/meshblock-2024/meshblock-2024.shp
+path_meshblocks=data/meshblock-2024.shp
 
 # check that data assets are in place
 for x in ${!path_@}; do
