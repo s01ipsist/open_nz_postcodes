bfa9a7503b34af32ec8deb9073bf680dcbbff2ce
Sam Giffney <samuelgiffney@gmail.com>
2025-12-10 16:09:43 +1100

    Maintenance chores (#22)

--- scripts/setup-koordinates-data.sh
+++ scripts/setup-koordinates-data.sh
@@ -6,7 +6,7 @@ echo "-- BEGIN $(date)"
 cd /app/scripts/koordinates
 mkdir -p /app/scripts/koordinates/data
 
-apt update && apt install -y python3 python3-pip python3.11-venv
+apt update && apt install -y python3 python3-pip python3-venv
 
 python3 -m venv onzp
 source onzp/bin/activate
